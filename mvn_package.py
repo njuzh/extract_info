8f71af99163b75d6b7dbd23d3e00849ccc7a4571
njuzh <383395862@qq.com>
2019-11-18 15:58:10 +0800

    add all_mvn_infomation

--- mvn_package.py
+++ mvn_package.py
@@ -4,9 +4,10 @@ import re
 
 start = time.time()
 
-exp_time = "3"
+exp_time = "4"
+localtime = time.asctime( time.localtime(time.time()) )
 projects_dir = "/root/repos/repos"+exp_time
-
+mvn_log_dir  = "/root/extract_info/mvn_log"
 all_projects_count = len(os.listdir(projects_dir))
 mvn_projects_count = 0
 delete_projects = []
@@ -22,6 +23,7 @@ for project_name in os.listdir(projects_dir):
         print(">>>>>>>>>>>>>>>>>>>>>>>>>>mvn package",project_name+">>>>>>>>>>>>>>")
         os.system("cd " +project_dir + "; mvn package -DskipTests") 
 end = time.time()
+
 print("=================================")
 print("all projects count:", all_projects_count)
 print("package projects count:", mvn_projects_count)
@@ -30,4 +32,14 @@ print("delete no maven projects:", ' '.join(delete_projects))
 print("time used:", end - start)
 print("=================================")
 
+with open(os.path.join(mvn_log_dir, "all_mvn_infomation.txt"), "a+") as f:
+    f.write("===============================\n")
+    f.write("experiment NO: " + exp_time + "\n")
+    f.write("experiment time: " + str(localtime) + "\n")
+    f.write("all projects count: " + str(all_projects_count) + "\n")
+    f.write("package projects count: " + mvn_projects_count)
+    f.write("package projects: " + ' '.join(analyzed_projects) + "\n")
+    f.write("delete no maven projects: " + ' '.join(delete_projects)+ "\n")
+    f.write("all time used: " +  str(end - start) +  "s\n" )
+    f.write("===============================\n")
 #sss
